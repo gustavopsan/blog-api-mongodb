c7ee9ef35d599dc9a2fe42b191983a0116c8abee
Gustavo P. Santana <gustavopsantana4@gmail.com>
2022-08-14 03:42:31 -0300

    Ajustando erro de cors

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const JWT = require('jsonwebtoken');
+const CORS = require('cors');
 const mongoConnection = require('./database/connection');
 const userModel = require('./models/user');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+    app.use(CORS());
+    next();
+});
 
 app.get('/', (req, res) => {
     res.json({
