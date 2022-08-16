3a2598a3bd863a9fee59f4d7f5f672c280efc709
Gustavo P. Santana <gustavopsantana4@gmail.com>
2022-08-16 12:07:25 -0300

    Adicionando mensagens de log na API

--- index.js
+++ index.js
@@ -36,17 +36,25 @@ app.get('/', (req, res) => {
 app.post('/createUser', async (req, res) => {
     const { name, email, password, passwordConfirm, avatar, active } = req.body;
 
+    console.log("Blog API MongoDB - createUser - Iniciando criação de novo usuário");
+
     if (password !== passwordConfirm) {
+        console.log("Blog API MongoDB - createUser - Senhas não conferem");
+
         res.json({
             errorId: 'register_01',
             message: 'Password does not match'
         });
     } else if (!validateEmail(email)) {
+        console.log("Blog API MongoDB - createUser - Email com formato inválido");
+
         res.json({
             errorId: 'register_02',
             message: 'Invalid email format'
         });
     } else if (!validatePassword(password)) {
+        console.log("Blog API MongoDB - createUser - Senha com formato inválido");
+
         res.json({
             errorId: 'register_03',
             message: 'Invalid password format'
@@ -55,11 +63,15 @@ app.post('/createUser', async (req, res) => {
         createUser(name, email, password, avatar, active)
             .then(response => {
                 if (response.hasOwnProperty('keyValue')) {
+                    console.log("Blog API MongoDB - createUser - Email já cadastrado");
+
                     return res.json({
                         errorId: 'register_04',
                         message: 'Email already exists'
                     });
                 } else {
+                    console.log("Blog API MongoDB - createUser - Usuário criado com sucesso");
+
                     return res.json({
                         message: 'User created successfully'
                     });
@@ -72,18 +84,26 @@ app.post('/createUser', async (req, res) => {
 app.post('/authenticate', async (req, res) => {
     const { email, password } = req.body;
 
+    console.log("Blog API MongoDB - authenticate - Iniciando autenticação de usuário");
+
     authenticateUser(email, password).then(response => {
         if(response.errorId == 'auth_01'){
+            console.log("Blog API MongoDB - authenticate - Usuário não encontrado com o email informado");
+
             res.json({
                 errorId: 'auth_01',
                 message: 'User not found'
             });
         } else if(response.errorId == 'auth_02'){
+            console.log("Blog API MongoDB - authenticate - Senha incorreta");
+
             res.json({
                 errorId: 'auth_02',
                 message: 'Password does not match'
             });
         } else if(response.errorId == 'auth_03'){
+            console.log("Blog API MongoDB - authenticate - Usuário inativo");
+
             res.json({
                 errorId: 'auth_03',
                 message: 'User is not active'
@@ -91,12 +111,16 @@ app.post('/authenticate', async (req, res) => {
         } else {
             const token = JWT.sign({ id: response._id }, process.env.SECRET, { expiresIn: '1m' });
 
+            console.log("Blog API MongoDB - authenticate - Usuário autenticado com sucesso");
+
             res.json({
                 userId: response._id,
                 token
             });
         }
     }).catch(error => {
+        console.log("Blog API MongoDB - authenticate - Erro ao autenticar usuário", error);
+
         res.json(error);
     })
 })
@@ -119,6 +143,8 @@ app.post('/checkSession', async (req, res) => {
 })
 
 app.get('/getUsers', async (req, res) => {
+    console.log("Blog API MongoDB - getUsers - Retornando todos os usuários");
+
     try {
         const users = await userModel.find();
         res.json(users);
@@ -130,13 +156,19 @@ app.get('/getUsers', async (req, res) => {
 app.post('/updateUser', async (req, res) => {
     const { id, key, value } = req.body;
 
+    console.log("Blog API MongoDB - updateUser - Atualizando usuário");
+
     updateUser(id, key, value).then(response => {
+        console.log("Blog API MongoDB - updateUser - Usuário atualizado com sucesso");
+
         res.json(response);
     }).catch(error => {
+        console.log("Blog API MongoDB - updateUser - Erro ao atualizar usuário", error);
+
         res.json(error);
     })
 })
 
 app.listen(PORT, () => {
-    console.log('Server started on port ' + PORT);
+    console.log('Blog API MongoDB - Server started on port ' + PORT);
 })
